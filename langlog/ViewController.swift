e8b0333c21a5d0aa690c8bac7c6fb3dbc56ebba6
Anton Van Eechaute <anton@wantedly.com>
2018-08-17 23:25:28 +0900

    create project

---
+++ langlog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  langlog
+//
+//  Created by anton van eechaute on 2018/08/17.
+//  Copyright © 2018 Anton Van Eechaute. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
