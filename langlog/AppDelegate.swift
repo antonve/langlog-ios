9850d5cb9b7c0fbd7223cffb38d537ea20383533
Anton Van Eechaute <anton@wantedly.com>
2018-08-18 00:16:23 +0900

    add view controller programmatically

--- langlog/AppDelegate.swift
+++ langlog/AppDelegate.swift
@@ -10,6 +10,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
     ) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = UIColor(hex: 0xf5f8f1)
+        window!.rootViewController = viewController
+        window!.makeKeyAndVisible()
+
         return true
     }
 }
