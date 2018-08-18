6a499f921390d4b8e52a59cad322bc4bd3babab6
Anton Van Eechaute <anton@wantedly.com>
2018-08-18 16:19:43 +0900

    add login button

--- langlog/App/Login/LoginViewController.swift
+++ langlog/App/Login/LoginViewController.swift
@@ -1,6 +1,16 @@
 import UIKit
+import SnapKit
+import Then
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, ViewConstructor {
+    struct Const {
+        static let padding: CGFloat = 50
+    }
+
+    private let loginButton = LoginButton().then {
+        $0.setTitle("Log in", for: .normal)
+    }
+
     override func viewWillDisappear(_ animated: Bool) {
         navigationController?.setNavigationBarHidden(false, animated: true)
     }
@@ -8,12 +18,19 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
+        setupConstraints()
     }
 
     func setupViews() {
         navigationController?.setNavigationBarHidden(true, animated: false)
         view.backgroundColor = UIColor(hex: 0xf5f8f1)
+
+        view.addSubview(loginButton)
     }
 
-    func setupViewConstraints() {}
+    func setupConstraints() {
+        loginButton.snp.makeConstraints {
+            $0.left.right.bottom.equalToSuperview().inset(Const.padding)
+        }
+    }
 }
---
+++ langlog/UI/Components/LoginButton.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+class LoginButton: UIButton, ViewConstructor {
+    struct Const {
+        static let height: CGFloat = 48
+    }
+
+    init() {
+        super.init(frame: .zero)
+        setupViews()
+        setupConstraints()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func setupViews() {
+        backgroundColor = .blue
+        layer.cornerRadius = 10
+    }
+
+    func setupConstraints() {
+        snp.makeConstraints {
+            $0.height.equalTo(Const.height)
+        }
+    }
+}
