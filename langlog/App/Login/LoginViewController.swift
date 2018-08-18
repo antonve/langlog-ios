692acdf198ca1b9f53634848f693e14ec8b3ab72
Anton Van Eechaute <anton@wantedly.com>
2018-08-18 16:06:14 +0900

    hide nav bar

--- langlog/App/Login/LoginViewController.swift
+++ langlog/App/Login/LoginViewController.swift
@@ -1,12 +1,17 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    override func viewWillDisappear(_ animated: Bool) {
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
     }
 
     func setupViews() {
+        navigationController?.setNavigationBarHidden(true, animated: false)
         view.backgroundColor = UIColor(hex: 0xf5f8f1)
     }
 
