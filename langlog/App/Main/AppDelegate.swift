83adc1d4aeb7ae5961df0fe6782163e405326b09
Anton Van Eechaute <anton@wantedly.com>
2018-11-10 14:52:28 +0900

    use new coordinator in app delegate

--- langlog/App/Main/AppDelegate.swift
+++ langlog/App/Main/AppDelegate.swift
@@ -1,23 +1,22 @@
+import Swinject
+import SwinjectAutoregistration
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    var window: UIWindow?
-
+    // swiftlint:disable:next implicitly_unwrapped_optional
     var coordinator: AppCoordinator!
+
+    var assembler = Assembler([
+        AppAssembly(),
+    ])
 
     func application(
         _: UIApplication,
         didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-        coordinator = AppCoordinator()
-
-        let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = coordinator.rootViewController
-        window.makeKeyAndVisible()
-        self.window = window
-
-        coordinator.presented(from: nil)
+        coordinator = AppCoordinator(resolver: assembler.resolver)
+        coordinator.start()
 
         return true
     }
