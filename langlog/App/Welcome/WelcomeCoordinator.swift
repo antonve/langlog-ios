167c4f06fb229bb4876de1698143fdea2efee7a8
Anton Van Eechaute <anton@wantedly.com>
2018-11-10 15:34:12 +0900

    add welcome coordinator

---
+++ langlog/App/Welcome/WelcomeCoordinator.swift
@@ -0,0 +1,30 @@
+import Swinject
+
+class WelcomeCoordinator: NavigationCoordinator {
+    var navigationController = UINavigationController()
+    var resolver: Resolver
+
+    init(resolver: Resolver) {
+        self.resolver = resolver
+    }
+
+    func start() {
+        let callback = WelcomeViewController.Callback(
+            didTapLogin: { print("login") },
+            didTapRegister: { [weak self] in self?.showSignup() }
+        )
+        let viewController = WelcomeViewController(callback: callback)
+        viewController.reactor = WelcomeReactor()
+
+        navigationController.pushViewController(viewController, animated: false)
+    }
+
+    private func showSignup() {
+        let callback = SignupViewController.Callback(
+            didTapSubmit: { print("submit") }
+        )
+        let viewController = SignupViewController(callback: callback)
+
+        navigationController.pushViewController(viewController, animated: true)
+    }
+}
