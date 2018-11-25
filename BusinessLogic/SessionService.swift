7bbe7764d0ac8aaf09c627193961c3fb70d0353b
Anton Van Eechaute <anton@wantedly.com>
2018-11-25 13:46:20 +0900

    move string library extension functions

--- BusinessLogic/SessionService.swift
+++ BusinessLogic/SessionService.swift
@@ -72,14 +72,3 @@ extension SessionService: TargetType {
         return ["Content-type": "application/json"]
     }
 }
-
-// @TODO: move these
-private extension String {
-    var urlEscaped: String {
-        return addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!
-    }
-
-    var utf8Encoded: Data {
-        return data(using: .utf8)!
-    }
-}
